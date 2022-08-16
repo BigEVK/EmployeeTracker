a70b6a3d5fc92e07f9e0a126e585315fe9ea8ecf
Eric VanKirk <eric@ericvankirk.com>
2022-08-16 15:17:28 -0500

    seed, schema, and db setup

---
+++ db/db.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS deptStore;
+CREATE DATABASE deptStore;
+USE deptStore;
---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+INSERT INTO department (name)
+VALUES
+('Men'),
+('Women'),
+('Children'),
+('Toys'),
+('Shoes'),
+('Perfume'),
+('Sports');
+
+INSERT INTO role (title, salary)
+VALUES
+('Regional Manager', '85,000'),
+('Store Manager', '65,000'),
+('Depatment Manager', '45,000'),
+('Clerk', '12.50');
+
+INSERT INTO employee (first_name, last_name)
+VALUES
+('John', 'Jameson'),
+('Lightning', 'McQueen'),
+('Buzz', 'Lightyear'),
+('Franken', 'Stein'),
+('Hans', 'Gruber'),
+('Carl', 'Brutananadilewski'),
+('Kyle', 'Broflovski'),
+('Kenny', 'McCormick'),
+('Eric', 'Cartman'),
+('Stan', 'Marsh'),
+('Clyde', 'Donovan'),
+('Craig', 'Tucker'),
+('Randy', 'Marsh'),
+('Terrence', 'Phillips');
