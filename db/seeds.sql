d002fd43bf52b047e8ae06c823f5dfaf5b0377c5
Eric VanKirk <eric@ericvankirk.com>
2022-08-16 16:55:21 -0500

    added express mysql2 packages.json middleware

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,19 +15,19 @@ VALUES
 ('Depatment Manager', '45000'),
 ('Clerk', '12.50');
 
-INSERT INTO employee (first_name, last_name)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('John', 'Jameson'),
-('Lightning', 'McQueen'),
-('Buzz', 'Lightyear'),
-('Franken', 'Stein'),
-('Hans', 'Gruber'),
-('Carl', 'Brutananadilewski'),
-('Kyle', 'Broflovski'),
-('Kenny', 'McCormick'),
-('Eric', 'Cartman'),
-('Stan', 'Marsh'),
-('Clyde', 'Donovan'),
-('Craig', 'Tucker'),
-('Randy', 'Marsh'),
-('Terrence', 'Phillips');
+('John', 'Jameson', 1, null),
+('Lightning', 'McQueen', 2, 1),
+('Buzz', 'Lightyear', 2, 1),
+('Franken', 'Stein', 3, 2),
+('Hans', 'Gruber', 3, 2),
+('Carl', 'Brutananadilewski', 3, 2),
+('Kyle', 'Broflovski', 3, 2),
+('Kenny', 'McCormick', 3, 2),
+('Eric', 'Cartman', 4, 3),
+('Stan', 'Marsh', 4, 3),
+('Clyde', 'Donovan', 4, 3),
+('Craig', 'Tucker', 4, 3),
+('Randy', 'Marsh', 4, 3),
+('Terrence', 'Phillips', 4, 3);
--- server.js
+++ server.js
@@ -5,3 +5,18 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const inputCheck = require('./utils/inputCheck');
 
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// connect to dattabase
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'R00t',
+        database: 'dept_store'
+    },
+    console.log('Connected to the dept_store database')
+);
+
